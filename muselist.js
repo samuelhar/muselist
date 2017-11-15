72e1f8039fc958fbd7dd9019c0283eb923f55a1c
Sam Foreman <32251124+sam-foreman@users.noreply.github.com>
2017-11-14 23:46:07 -0500

    Update muselist.js

--- muselist.js
+++ muselist.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 require('dotenv').config();
 
+var mongodb = require("mongodb");
 var app = express();
 
 app.disable('x-powered-by');
@@ -28,6 +29,48 @@ app.get('/', function(req, res){
   res.render('home');
 });
 
+app.get('/list', function(req, res) {
+  var MongoClient = mongodb.MongoClient;
+  var url = 'mongodb://localhost:27017/test';
+  MongoClient.connect(url, function(err, db) {
+    if (err){
+      console.log('Unable to connect to db', err);
+    } else {
+      console.log('Connection established');
+      var cl = db.collection('test');
+      cl.find({}).toArray(function (err, result) {
+        if (err) {
+          res.send(err);
+        } else if (result.length > 0) {
+          res.render('collectionlist', {
+            "collectionlist": JSON.stringify(result)
+          });
+        } else {
+          res.render("empty");
+        }
+        db.close();
+      });
+    }
+  });
+});
+
+app.get('/clear', function(req, res) {
+  var MongoClient = mongodb.MongoClient;
+  var url = 'mongodb://localhost:27017/test';
+  MongoClient.connect(url, function(err, db) {
+    if (err) {
+      console.log('Unable to connect to db', err);
+    } else {
+      console.log('Connection established');
+      var cl = db.collection('test');
+      cl.remove({});
+      res.render('clear');
+    }
+    db.close();
+  });
+});
+
+
 app.use(function(req, res, next){
 	console.log("Looking for URL : " + req.url);
   next();
@@ -44,6 +87,37 @@ app.post('/process', function(req, res){
     return spotifyApi.searchTracks('artist:'+ req.body.artist, {limit: 5})
   }).then(function(data) {
     res.render('search-results', {artist: req.body.artist, tracks: data.body.tracks.items});
+
+    var len = data.body.tracks.items.length;
+    if (len >= 5)
+    {
+      var artistString = req.body.artist;
+      var track1 = data.body.tracks.items[0].name;
+      var track2 = data.body.tracks.items[1].name;
+      var track3 = data.body.tracks.items[2].name;
+      var track4 = data.body.tracks.items[3].name;
+      var track5 = data.body.tracks.items[4].name;
+
+      var MongoClient = mongodb.MongoClient;
+      var url = 'mongodb://localhost:27017/test';
+      MongoClient.connect(url, function(err, db) {
+        if (err){
+          console.log("Unable to connect to db", err);
+        } else {
+          console.log("Connection established");
+          var cl = db.collection("test");
+          cl.insert([{"artist": artistString, "track1": track1, "track2": track2
+        , "track3": track3, "track4":track4, "track5": track5}]);
+        }
+       db.close();
+
+      });
+    }
+    else
+    {
+      console.log("not enough tracks...");
+    }
+
 
   }).catch(function(err) {
     console.log('Unfortunately, something has gone wrong.', err.message);
