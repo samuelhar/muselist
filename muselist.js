1525d708132354896b28960186215c8836601672
Ivorine Do <ido@dhcp-wifi-8021x-155-41-9-117.bu.edu>
2017-11-29 18:02:10 -0500

    auth working

--- muselist.js
+++ muselist.js
@@ -1,33 +1,44 @@
+/* CS 411 Muselist
+*/
+
 var express = require('express');
-
+var request = require('request');
+var path = require('path');
 require('dotenv').config();
-
+var querystring = require('querystring');
 var app = express();
-
+var cookieParser = require('cookie-parser');
 app.disable('x-powered-by');
-
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
-
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-
 app.use(require('body-parser').urlencoded({extended: true}));
-
 var formidable = require('formidable');
-
 var SpotifyWebApi = require('spotify-web-api-node');
 
-var spotifyApi = new SpotifyWebApi({
-  clientId :'5045889d21114f6d9ef7f7b4501ea6ee',
-  clientSecret :'db156bc04c0d4cdab6505249eef4d0cd',
-  
-});
+var client_id = ''; // Your client id
+var client_secret = ''; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; 
 
+
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+var stateKey = 'spotify_auth_state';
 
 var playlistId;
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8888);
 
+app.use(express.static(__dirname + '/public'))
+   .use(cookieParser());
 
 var array_artist = [];
 var array_song = [];
@@ -37,7 +48,7 @@ var song_id = "http://open.spotify.com/embed?uri=spotify:track:";
 
 
 app.get('/', function(req, res){
-  res.render('home');
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.get('/login', function(req, res) {
@@ -49,13 +60,79 @@ app.get('/login', function(req, res) {
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
-      clientId: clientId,
+      client_id: client_id,
       scope: scope,
       redirect_uri: redirect_uri,
       state: state
     }));
 });
 
+app.get('/continue', function(req, res){
+  res.render('home');
+});
+
+app.get('/callback', function(req, res) {
+
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    res.clearCookie(stateKey);
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
+
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+
+        var access_token = body.access_token,
+            refresh_token = body.refresh_token;
+
+        var options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+
+        // use the access token to access the Spotify Web API
+        request.get(options, function(error, response, body) {
+          console.log(body);
+        });
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect('/#' +
+          querystring.stringify({
+            access_token: access_token,
+            refresh_token: refresh_token
+          }));
+      } else {
+        res.redirect('/#' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
+    });
+  }
+  //res.redirect('https://google.com');
+});
 
 app.use(function(req, res, next){
 	console.log("Looking for URL : " + req.url);
